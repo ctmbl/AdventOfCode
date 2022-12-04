50e89a47e2277e6eb553686235052ab141439347
ctmbl <mabileau.clement@gmail.com>
2022-12-03 20:54:32 -0500

    Improve template.c

--- template.c
+++ template.c
@@ -11,6 +11,14 @@ int main(int argc, char** argv){
 		filename = argv[1];
 	}
 	FILE* input_file = fopen(filename, "r");
+	char* line = NULL;
+	int i = 0;
+	int c = 0;
+	while(1){
+		c = getline(&line, &i, input_file);
+		if(c == -1)
+			break;
+	}
 	fclose(input_file);
 	return 0;
 }
