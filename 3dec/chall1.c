0ec58a3b970e406f2b01368aaaea5447b121a597
ctmbl <mabileau.clement@gmail.com>
2022-12-03 21:03:45 -0500

    Add in function and line striping

--- 3dec/chall1.c
+++ 3dec/chall1.c
@@ -1,9 +1,13 @@
 #!/usr/bin/tcc -run
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 
 char* FILENAME = "input.txt";
+
+bool in(char chr, char* str);
 
 int main(int argc, char** argv){
 	char* filename = FILENAME;
@@ -18,7 +22,25 @@ int main(int argc, char** argv){
 		c = getline(&line, &i, input_file);
 		if(c == -1)
 			break;
+		c--;
+		line[c] = '\0';
+		if(strlen(line)%2 != 0){
+			puts("error on line length!!!!");
+			exit(1);
+		}
+
+
+		free(line);
+		line = NULL;
 	}
 	fclose(input_file);
 	return 0;
+}
+
+bool in(char chr, char* str){
+       for(char* c = str; *c != '\0'; c++){
+	       if(*c == chr)
+		       return true;
+       }
+       return false;
 }
