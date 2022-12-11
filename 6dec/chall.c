19856ac048b86c77fee0c838e0b8b1c903391f56
ctmbl <mabileau.clement@gmail.com>
2022-12-10 19:51:53 -0500

    Add 6dec chall part2

--- 6dec/chall.c
+++ 6dec/chall.c
@@ -6,6 +6,9 @@
 
 static char* FILENAME = "input.txt";
 
+static inline packet_start(){
+}
+
 int main(int argc, char** argv){
 	char* filename = FILENAME;
 	if (argc > 1) {
@@ -20,23 +23,28 @@ int main(int argc, char** argv){
 	if(c <= 1)
 		exit(1);
 
-	char* marker = line;
+	size_t packet_start = 0;
+	size_t msg_start = 0;
+	size_t curr = 0;
 	size_t k = 1;
-	while(*(line+k) != '\0' && *(line+k) != '\n'){
-		// check that every character between marker and line+k are diff, increment marker else
-		for(char* p = marker; p < line+k; p++){
-			if(*p == *(line+k))
-				marker = p+1;
+	while(*(line+k) != '\0' && *(line+k) != '\n' && !(packet_start && msg_start)){
+		// check that every character between line+curr and line+k are diff, increment curr else
+		for(size_t p = curr; p < k; p++){
+			if(*(line+p) == *(line+k))
+				curr = p+1;
 		}
-		// if we found 4 diff char break
-		if(line+k-marker == 4)
-			break;
+		// if we found 4 diff chars found packet_start
+		if(k-curr+1 == 4 && !packet_start)
+			packet_start = k+1;
+		
+		if(k-curr+1 == 14 && !msg_start)
+			msg_start = k+1;
 		k++;
 	}
 	free(line);
 	line = NULL;
 	i = 0;
-	printf("%d\n", k);
+	printf("%d\n%d\n", packet_start, msg_start);
 	fclose(input_file);
 	return 0;
 }
