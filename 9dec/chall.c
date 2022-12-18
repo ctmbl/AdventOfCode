97956b813534681ca9d8fb407bf7f7dba9851a48
ctmbl <mabileau.clement@gmail.com>
2022-12-18 17:03:31 -0500

    9dec: add movements to head

--- 9dec/chall.c
+++ 9dec/chall.c
@@ -5,6 +5,16 @@
 #include <string.h>
 
 static char* FILENAME = "input.txt";
+typedef struct pos {
+	int x;
+	int y;
+} pos;
+
+static pos head = {0,0};
+static pos tail = {0,0};
+
+void move(char dir);
+void follow(pos* trace, size_t* tot);
 
 int main(int argc, char** argv){
 	char* filename = FILENAME;
@@ -12,15 +22,22 @@ int main(int argc, char** argv){
 		filename = argv[1];
 	}
 	FILE* input_file = fopen(filename, "r");
+	pos* trace = NULL;
+	char dir = ' ';
+	int steps = 0;
+
 	char* line = NULL;
 	size_t i = 0;
 	int c = 0;
 	size_t tot = 0;
-	while(1){
-		c = getline(&line, &i, input_file);
-		if(c == -1)
-			break;
-		/* code goes here */
+	while(-1 != (c = getline(&line, &i, input_file))){
+		sscanf(line,"%c %d", &dir, &steps);
+		while(steps != 0){
+			move(dir);
+			follow(trace, &tot);
+			steps--;
+		}
+		
 		free(line);
 		line = NULL;
 		i = 0;
@@ -28,4 +45,22 @@ int main(int argc, char** argv){
 	printf("%d\n", tot);
 	fclose(input_file);
 	return 0;
+}
+
+void move(char dir){
+	switch (dir) {
+		case 'U': head.y++;
+			  break;
+		case 'D': head.y--;
+			  break;
+		case 'R': head.x++;
+			  break;
+		case 'L': head.x--;
+			  break;
+		default : printf("'%c' is not a direction\n", dir);
+			  break;
+	}
+	//printf("%d %d\n", head.x, head.y);
+}
+void follow(pos* trace, size_t* tot){
 }
