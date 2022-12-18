4744fec2c774179d8c81dce01859cfb696e8ad05
ctmbl <mabileau.clement@gmail.com>
2022-12-18 16:32:55 -0500

    Add 9dec code

---
+++ 9dec/chall.c
@@ -0,0 +1,31 @@
+#!/usr/bin/tcc -run
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static char* FILENAME = "input.txt";
+
+int main(int argc, char** argv){
+	char* filename = FILENAME;
+	if (argc > 1) {
+		filename = argv[1];
+	}
+	FILE* input_file = fopen(filename, "r");
+	char* line = NULL;
+	size_t i = 0;
+	int c = 0;
+	size_t tot = 0;
+	while(1){
+		c = getline(&line, &i, input_file);
+		if(c == -1)
+			break;
+		/* code goes here */
+		free(line);
+		line = NULL;
+		i = 0;
+	}
+	printf("%d\n", tot);
+	fclose(input_file);
+	return 0;
+}
