a9984996ae523ead36021aa8e23e4e7f930653c6
ctmbl <mabileau.clement@gmail.com>
2023-12-02 16:39:09 +0100

    Add 1dec part2

---
+++ 2023/01dec/chall.c
@@ -0,0 +1,83 @@
+#!/usr/bin/tcc -run
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+//static char* FILENAME = "input.ex.txt";
+static char* FILENAME = "input.txt";
+
+int is_digit_part1(char* c){
+	return isdigit(*c);
+}
+
+int is_digit_in_letters(char* c, char* d){
+	char digits[9][6] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+	for(int i = 0; i < 9; i++){
+		if(strncmp(digits[i], c, strlen(digits[i])) == 0){
+			*d = (char)0x31+i;
+			//printf("DEBUG: found digit in letters: %s == %c\n", c, *d);
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int is_digit_part2(char* c, char* d){
+	if(isdigit(*c)){
+		*d = *c;
+		//printf("DEBUG: found digit it's %c\n", *d);
+		return 1;
+	}
+	return is_digit_in_letters(c, d);
+}
+
+int main(int argc, char** argv){
+	char* filename = FILENAME;
+	if (argc > 1) {
+		filename = argv[1];
+	}
+	FILE* input_file = fopen(filename, "r");
+	char* line = NULL;
+	size_t i = 0;
+	int c = 0;
+	size_t tot = 0;
+	while(1){
+		c = getline(&line, &i, input_file);
+		if(c == -1)
+			break;
+
+		char* start = line;
+		char* end = line + strlen(line)-1 -1;
+		printf("\nDEBUG: line:=%s - start: '%c' - end '%c'\n", line, *start, *end);
+		char d1;
+		char d2;
+		while(1){
+			if(!is_digit_part2(start, &d1)){
+				start++;
+				continue;
+			}
+			//printf("found start=%c\n", d1);
+			if(!is_digit_part2(end, &d2)){
+				end--;
+				continue;
+			}
+			//printf("DEBUG: line after modif: %s\n", line);
+			//printf("found end=%c\n", d2);
+			break;
+		}
+		char nb[3] = {'a','a', '\0'};
+		nb[0] = d1;
+		nb[1] = d2;
+		printf("DEBUG nb=%s\n", nb);
+		printf("DEBUG: %d = %d + %s\n", tot+ atoi(nb), tot, nb );
+		tot += atoi(nb);
+		free(line);
+		line = NULL;
+		i = 0;
+	}
+	printf("%d\n", tot);
+	fclose(input_file);
+	return 0;
+}
