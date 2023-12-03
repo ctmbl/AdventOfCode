db16c0c9118f07076fcaee4b67e9cb628e6a1632
ctmbl <mabileau.clement@gmail.com>
2023-12-03 14:50:28 +0100

    update template.c

--- template.c
+++ template.c
@@ -13,18 +13,25 @@ int main(int argc, char** argv){
 	}
 	FILE* input_file = fopen(filename, "r");
 	char* line = NULL;
-	size_t i = 0;
-	int c = 0;
+	size_t buf_size = 0;
+
 	size_t tot = 0;
 	while(1){
-		c = getline(&line, &i, input_file);
-		if(c == -1)
+		int len = getline(&line, &buf_size, input_file);
+		if(len < 2) // allow to stop on empty lines OR and EOF errors
 			break;
+
+		if(line[len-1] == '\n'){
+			line[len-1] = '\0';
+			len--;
+		}
+
 		/* code goes here */
-		free(line);
-		line = NULL;
-		i = 0;
 	}
+	free(line);
+	line = NULL;
+	buf_size = 0;
+
 	printf("%d\n", tot);
 	fclose(input_file);
 	return 0;
