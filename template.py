4ade2faf39439a7cc941a05f73a54d7be6e4c830
Clement Mabileau <cmabileau@cesi.fr>
2025-12-01 12:20:44 +0100

    Add python template

---
+++ template.py
@@ -0,0 +1,44 @@
+import logging
+
+LOGGER = logging.getLogger()
+
+def get_input_lines(filename):
+    lines = None
+    with open(filename, "r") as f:
+        lines = f.readlines()
+    return lines
+
+
+def main(lines):
+    # Code goes here
+    pass
+
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-x",
+        "--example",
+        action="store_true",
+        help="Run on input.ex.txt",
+    )
+    parser.add_argument(
+        "-i",
+        "--input",
+        default="input.txt",
+        help="Pass a custom file as input",
+    )
+    parser.add_argument(
+        "-d",
+        "--debug",
+        action="store_true",
+        help="Change logging level to debug",
+    )
+    ARGS = parser.parse_args()
+    input_filename = "input.ex.txt" if ARGS.example else ARGS.input
+    level = logging.DEBUG if ARGS.debug else logging.INFO
+
+    logging.basicConfig(level = level)
+
+    main(get_input_lines(input_filename))
