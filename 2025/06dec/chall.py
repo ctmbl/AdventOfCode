eb1684409a2c9b17d4958cb0a8d6e66c80f4a719
ctmbl <mabileau.clement@gmail.com>
2025-12-06 16:27:53 +0100

    Add 06dec 2025 Part 2

--- 2025/06dec/chall.py
+++ 2025/06dec/chall.py
@@ -14,22 +14,46 @@ def main(lines):
     part1 = 0
     part2 = 0
 
-    table = list(map(lambda l : list(map(int, l)), [lines[i].strip().split() for i in range(len(lines)-1)]))
+    table_p1 = list(map(lambda l : list(map(int, l)), [lines[i].strip().split() for i in range(len(lines)-1)]))
     operations = lines[-1].strip().split()
 
-    for i in range(len(table[0])):
+    # Part1
+    for i in range(len(table_p1[0])):
         n = None
         if operations[i] == '+':
             n = 0
-            for j in range(len(table)):
-                n += table[j][i]
+            for j in range(len(table_p1)):
+                n += table_p1[j][i]
         elif operations[i] == '*':
             n = 1
-            for j in range(len(table)):
-                n *= table[j][i]
+            for j in range(len(table_p1)):
+                n *= table_p1[j][i]
         else:
             raise NotImplemented
         part1 += n
+
+    # Part2
+    table_p2 = [[]]
+    for i in range(len(lines[0].strip("\n"))):
+        col = [lines[j].strip("\n")[i] for j in range(len(lines)-1)]
+        if any(map(lambda c: c.isdigit(), col)):
+            table_p2[-1].append(int("".join(col).strip(" ")))
+            continue
+        table_p2.append([])
+
+    for i in range(len(table_p2)):
+        n = None
+        if operations[i] == '+':
+            n = 0
+            for j in range(len(table_p2[i])):
+                n += table_p2[i][j]
+        elif operations[i] == '*':
+            n = 1
+            for j in range(len(table_p2[i])):
+                n *= table_p2[i][j]
+        else:
+            raise NotImplemented
+        part2 += n
 
     LOGGER.info("Part1: %s", part1)
     LOGGER.info("Part2: %s", part2)
